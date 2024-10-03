9d714667490d5616959f58324e521a7bb992dfa8
Bharath420 <bharath.mdvr@gmail.com>
2024-10-03 14:29:38 -0400

    Updates

---
+++ precache-manifest.21087db7b3a6eb1d03d2e2a2652611ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3365cfd3c057c2a0fe5fd179f9000e75",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "c3ae2045054ade5e3c73",
+    "url": "/resume/static/css/main.ffbc83f5.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/resume/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/resume/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "c3ae2045054ade5e3c73",
+    "url": "/resume/static/js/main.125ea1b6.chunk.js"
+  },
+  {
+    "revision": "680d1dec47fbce27118d",
+    "url": "/resume/static/js/runtime~main.e1c15cea.js"
+  }
+]);
